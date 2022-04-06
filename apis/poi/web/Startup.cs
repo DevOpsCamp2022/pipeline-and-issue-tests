eff338c7c5e0afa55cf2fde575f5c0a29434ec27
Leo von Wyss <leo.vonwyss@zuehlke.com>
2022-04-06 14:45:28 +0200

    Add AppInsights (#67)

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -28,6 +28,9 @@ namespace poi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // The following line enables Application Insights telemetry collection.
+            services.AddApplicationInsightsTelemetry();
+
             services.AddControllers()
                 .AddNewtonsoftJson((options =>
                 {
